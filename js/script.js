00234de5e40be718734b5c7343d64a0a550e42da
Ant Iva <sg.antiiva@gmail.com>
2020-07-04 21:55:36 +0300

    scroll animation added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 window.onload = function() {
+    //NAVIGATION HIDING AFTER CLICK
     const navCheckbox = document.querySelector(".navigation__checkbox");
     const navList = document.querySelector(".navigation__list");
 
     navList.addEventListener("click", function(event) {
         navCheckbox.checked = false;
+    });
+
+    //SCROLL ANIMATION
+
+    //section-about Feature-Box moveInBottom
+    var waypoint1 = new Waypoint({
+        element: document.querySelector(".section-about"),
+        handler: function(direction) {
+            Array.from(document.querySelectorAll(".feature-box")).forEach(elem => {
+                elem.classList.add("feature-box--animated");
+            });
+        }
+    });
+
+    //section-steps steps__image-box moveInBottom
+    var waypoint2 = new Waypoint({
+        element: document.querySelector(".section-steps"),
+        handler: function(direction) {
+            document.querySelector(".steps__image-box").classList.add("steps__image-box--animated");
+        },
+        offset: '150px;'
     });
 }
