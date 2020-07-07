728b11f97c14cc249af5f498a15bbae3c9f02766
Ant Iva <sg.antiiva@gmail.com>
2020-07-07 11:15:08 +0300

    spinner fuctionality complete

--- js/script.js
+++ js/script.js
@@ -1,115 +1,128 @@
-window.onload = function() {
-    //NAVIGATION HIDING AFTER CLICK
-    var navCheckbox = document.querySelector(".navigation__checkbox");
-    var navList = document.querySelector(".navigation__list");
+//NAVIGATION HIDING AFTER CLICK
+var navCheckbox = document.querySelector(".navigation__checkbox");
+var navList = document.querySelector(".navigation__list");
 
-    navList.addEventListener("click", function(event) {
-        navCheckbox.checked = false;
-    });
+navList.addEventListener("click", function(event) {
+    navCheckbox.checked = false;
+});
 
-    //SCROLL ANIMATION
+//SCROLL ANIMATION
 
-    //section-about Feature-Box moveInBottom
-    var waypoint1 = new Waypoint({
-        element: document.querySelector(".section-about"),
-        handler: function(direction) {
-            Array.from(document.querySelectorAll(".feature-box")).forEach(function(elem) {
-                elem.classList.add("feature-box--animated");
-            });
+//section-about Feature-Box moveInBottom
+var waypoint1 = new Waypoint({
+    element: document.querySelector(".section-about"),
+    handler: function(direction) {
+        Array.from(document.querySelectorAll(".feature-box")).forEach(function(elem) {
+            elem.classList.add("feature-box--animated");
+        });
+    }
+});
+
+//section-steps steps__image-box moveInBottom
+var waypoint2 = new Waypoint({
+    element: document.querySelector(".section-steps"),
+    handler: function(direction) {
+        document.querySelector(".steps__image-box").classList.add("steps__image-box--animated");
+    },
+    offset: '150px;'
+});
+
+//SHOW SELECTED FILENAME BELOW THE UPLOAD FILE BTN
+var fileInput = document.querySelector(".form__file-input");
+var filenameLabel = document.querySelector(".form__file-name");
+fileInput.addEventListener("change", function(event) {
+    if (fileInput.files.length > 0) {
+        var filename = fileInput.files[0].name;
+        if (filename.length > 40) filename = "..." + filename.slice(-40);
+        filenameLabel.textContent = ".../" + filename;
+    } else filenameLabel.textContent = "";
+});
+
+
+//SEND FORM DATA via SMTPJS.COM
+var form = document.querySelector(".form");
+var spinnerCheckbox = document.querySelector(".spinner__checkbox");
+form.addEventListener("submit", function(event) {
+    //prepare data for email
+    var data = {};
+    var file;
+    Array.from(form.elements).forEach(function(elem) {
+        data[elem.name] = elem.value;
+        if (elem.name === "jobdesc-file") {
+            file = elem.files[0];
         }
     });
-
-    //section-steps steps__image-box moveInBottom
-    var waypoint2 = new Waypoint({
-        element: document.querySelector(".section-steps"),
-        handler: function(direction) {
-            document.querySelector(".steps__image-box").classList.add("steps__image-box--animated");
-        },
-        offset: '150px;'
+    //clear form fields
+    Array.from(form.elements).forEach(function(elem) {
+        elem.value = "";
     });
-
-    //SHOW SELECTED FILENAME BELOW THE UPLOAD FILE BTN
-    var fileInput = document.querySelector(".form__file-input");
-    var filenameLabel = document.querySelector(".form__file-name");
-    fileInput.addEventListener("change", function(event) {
-        if (fileInput.files.length > 0) {
-            var filename = fileInput.files[0].name;
-            if (filename.length > 40) filename = "..." + filename.slice(-40);
-            filenameLabel.textContent = ".../" + filename;
-        } else filenameLabel.textContent = "";
-    });
-
-
-    //SEND FORM DATA via SMTPJS.COM
-    var form = document.querySelector(".form");
-    form.addEventListener("submit", function(event) {
-        //prepare data for email
-        var data = {};
-        var file;
-        Array.from(form.elements).forEach(function(elem) {
-            data[elem.name] = elem.value;
-            if (elem.name === "jobdesc-file") {
-                file = elem.files[0];
-            }
-        });
-        //clear form fields
-        Array.from(form.elements).forEach(function(elem) {
-            elem.value = "";
-        });
-        filenameLabel.textContent = "";
-        //prepare email content
-        var emailSubject = data.name + " from " + data.company;
-        var emailBody = "<html><h3>Name: " +
-            data.name +
-            "</h3><h3>Company: " +
-            data.company +
-            "</h3><h4>Email: " +
-            data.email +
-            "</h4><p>Job description: " +
-            data.jobdesc +
-            "</p></html>";
-        //send email
-        if (file) {
-            readFileData(file)
-                .then(function(result) {
-                    Email.send({
-                            SecureToken: "19a97c53-3b33-4261-bce0-354578209dbd",
-                            To: 'what.s.web4you@gmail.com',
-                            From: data.email,
-                            Subject: emailSubject,
-                            Body: emailBody,
-                            Attachments: [{
-                                name: file.name,
-                                data: result
-                            }]
-                        }).then(function(message) {
-                            if (message === "OK") {
-                                window.location.hash = "#popup";
-                            } else window.location.hash = "#popup-failure";
-                        })
-                        .catch(function(error) {
-                            console.log(error);
-                        });
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-        } else Email.send({
-                SecureToken: "19a97c53-3b33-4261-bce0-354578209dbd",
-                To: 'what.s.web4you@gmail.com',
-                From: data.email,
-                Subject: emailSubject,
-                Body: emailBody
-            }).then(function(message) {
-                if (message === "OK") {
-                    window.location.hash = "#popup";
-                } else window.location.hash = "#popup-failure";
+    filenameLabel.textContent = "";
+    //prepare email content
+    var email = createEmail(data);
+    //send email
+    if (file) {
+        spinnerCheckbox.checked = true;
+        readFileData(file)
+            .then(function(result) {
+                Email.send({
+                        SecureToken: "19a97c53-3b33-4261-bce0-354578209dbd",
+                        To: 'what.s.web4you@gmail.com',
+                        From: email.from,
+                        Subject: email.subject,
+                        Body: email.body,
+                        Attachments: [{
+                            name: file.name,
+                            data: result
+                        }]
+                    }).then(function(message) {
+                        spinnerCheckbox.checked = false;
+                        if (message === "OK") {
+                            window.location.hash = "#popup";
+                        } else window.location.hash = "#popup-failure";
+                    })
+                    .catch(function(error) {
+                        spinnerCheckbox.checked = false;
+                        window.location.hash = "#popup-failure";
+                        console.log(error);
+                    });
             })
             .catch(function(error) {
+                spinnerCheckbox.checked = false;
+                window.location.hash = "#popup-failure";
                 console.log(error);
             });
-        event.preventDefault();
-    });
+    } else Email.send({
+            SecureToken: "19a97c53-3b33-4261-bce0-354578209dbd",
+            To: 'what.s.web4you@gmail.com',
+            From: email.from,
+            Subject: email.subject,
+            Body: email.body
+        }).then(function(message) {
+            spinnerCheckbox.checked = false;
+            if (message === "OK") {
+                window.location.hash = "#popup";
+            } else window.location.hash = "#popup-failure";
+        })
+        .catch(function(error) {
+            spinnerCheckbox.checked = false;
+            console.log(error);
+        });
+    event.preventDefault();
+});
+
+//prepare email content
+function createEmail(data) {
+    var subject = data.name + " from " + data.company;
+    var body = "<html><h3>Name: " +
+        data.name +
+        "</h3><h3>Company: " +
+        data.company +
+        "</h3><h4>Email: " +
+        data.email +
+        "</h4><p>Job description: " +
+        data.jobdesc +
+        "</p></html>";
+    return { from: data.email, subject: subject, body: body };
 }
 
 function readFileData(file) {
